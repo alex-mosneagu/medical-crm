1fbee37c80d3f71fd4bf7f09d01f693b9cd4bcac
alex-mosneagu <129764704+alex-mosneagu@users.noreply.github.com>
2023-07-26 20:57:14 +0300

    Test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,21 @@
 <template>
     <v-container fluid class="py-5 px-6 h-100 d-flex align-center justify-center" style="background-color: #45BFB3;">
       <v-card class="mx-auto pa-12" elevation="0" width="448" rounded="lg">
-        <h1 class="text-center text-h3">PsyHelp</h1>
+        <h1 class="text-center text-h3">
+          <img :src="logo" class="w-75" alt="">
+          <h6 class="text-h6 text-uppercase font-weight-bold">Login</h6>
+        </h1>
       </v-card>
     </v-container>
 </template>
   
 <script>
+  import logo from '../../public/logo.png'
   export default {
     data() {
       return{
-        name: 'Hello World'
+        name: 'Hello World',
+        logo: logo
       }
     }
   }

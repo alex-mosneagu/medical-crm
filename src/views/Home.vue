0acce9982c613e52bd04644254d175f20ece5cb0
alex-mosneagu <129764704+alex-mosneagu@users.noreply.github.com>
2023-07-26 21:20:43 +0300

    Login Form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,34 @@
 <template>
     <v-container fluid class="py-5 px-6 h-100 d-flex align-center justify-center" style="background-color: #45BFB3;">
       <v-card class="mx-auto pa-12" elevation="0" width="448" rounded="lg">
-        <h1 class="text-center text-h3">
+        <div class="text-center">
           <img :src="logo" class="w-75" alt="">
-          <h6 class="text-h6 text-uppercase font-weight-bold">Login</h6>
-        </h1>
+          <h6 class="text-h6 mt-5 text-uppercase font-weight-bold">LOGIN</h6>
+        </div>
+        <form class="mt-10" ref="form" @submit.prevent="login()">
+          <v-text-field
+            v-model="username"
+            name="username"
+            label="Username"
+            type="text"
+            placeholder="username"
+            required
+            rounded="pill"
+            variant="solo"
+          ></v-text-field>
+        
+          <v-text-field
+            v-model="password"
+            name="password"
+            label="Password"
+            type="password"
+            placeholder="password"
+            required
+            rounded="pill"
+            variant="solo"
+          ></v-text-field>
+          <v-btn type="submit" class="mt-4" color="primary" value="log in">Login</v-btn>
+        </form>
       </v-card>
     </v-container>
 </template>
